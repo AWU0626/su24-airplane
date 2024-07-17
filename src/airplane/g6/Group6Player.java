fe36c357eaf5929423c3d7e6ee1f00b7a3e5947d
Aaron Wu <awu0626@seas.upenn.edu>
2024-07-16 23:11:29 -0400

    improvised

--- src/airplane/g6/Group6Player.java
+++ src/airplane/g6/Group6Player.java
@@ -1,10 +1,17 @@
 package airplane.g6;
 
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import airplane.sim.Plane;
 import airplane.sim.Player;
 import org.apache.log4j.Logger;
+
+import javax.sound.sampled.Line;
 
 public class Group6Player extends airplane.sim.Player {
     private Logger logger = Logger.getLogger(this.getClass()); // for logging
@@ -19,16 +26,52 @@ public class Group6Player extends airplane.sim.Player {
         logger.info("Start new game");
     }
 
+//    private double[] movePlane(ArrayList<Plane> planes, int round, double[] bearings, int[] delay) {
+//        int size = planes.size();
+//
+//
+//    }
+
+    private int getQuadrant(double bearing) {
+        if (bearing >= 0 && bearing < 90) {
+            return 1;
+        } else if (bearing >= 90 && bearing < 180) {
+            return 2;
+        } else if (bearing >= 180 && bearing < 270) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
     @Override
     public double[] updatePlanes(ArrayList<Plane> planes, int round, double[] bearings) {
         int size = planes.size();
-        int[] delay = new int[planes.size()];
+        int[] delay = new int[size];
 
+        Map<Integer, Integer> quadrantDelayMap = new HashMap<>();
+        quadrantDelayMap.put(1, 0);
+        quadrantDelayMap.put(2, 0);
+        quadrantDelayMap.put(3, 10);
+        quadrantDelayMap.put(4, 10);
+
+        // Determine the quadrant for each plane and assign delay
         for (int i = 0; i < size; i++) {
-            delay[i] = 10 * i;
+            delay[i] = 20 * i;
+            delay[i] += Math.max(0, delay[i] - quadrantDelayMap.get(getQuadrant(bearings[i])));
         }
 
-        // single plane
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                if (j != i) {
+                    if (planes.get(i).getLocation() == planes.get(j).getLocation()) {
+                        delay[j] += 10;
+                    }
+                }
+            }
+        }
+
+// single plane
         if (size == 1) {
             if (bearings[0] == -2) return bearings;
             bearings[0] = calculateBearing(planes.get(0).getLocation(), planes.get(0).getDestination());
@@ -42,24 +85,52 @@ public class Group6Player extends airplane.sim.Player {
             // delay
             if (round >= delay[i]) {
                 bearings[i] = Player.calculateBearing(planes.get(i).getLocation(), planes.get(i).getDestination());
+                if (bearings[i] >= 360) bearings[i] -= 360;
 
                 for (int j = 0; j < size; j++) {
-                    if (j != i) {
+                    if (j > i) {
                         Plane p1 = planes.get(i);
                         Plane p2 = planes.get(j);
 
-                        if (p1.getLocation().distance(p2.getLocation()) <= 50) {
+                        if (p1.getLocation().distance(p2.getLocation()) <= 100) {
                             double newBearing = Player.calculateBearing(planes.get(i).getLocation(), planes.get(i).getDestination()) + 10;
                             double multiplier = planes.get(i).getLocation().distance(planes.get(i).getDestination());
                             multiplier /= 1 + planes.get(i).getLocation().distance(planes.get(i).getDestination());
 
-                            if (Math.abs(newBearing - bearings[i]) >= 10) { bearings[i] += 9 * multiplier; }
-                        } else if (p1.getLocation().distance(p2.getLocation()) <= 20) {
+                            if (Math.abs(newBearing - bearings[i]) >= 10) {
+                                Line2D line1 = new Line2D.Double(p1.getLocation().getX(), p1.getLocation().getY(), p1.getDestination().getX(), p1.getDestination().getY());
+                                Line2D line2 = new Line2D.Double(p2.getLocation().getX(), p2.getLocation().getY(), p2.getDestination().getX(), p2.getDestination().getY());
+
+                                if (line2.intersectsLine(line1) ||
+                                        (Math.abs(bearings[i] - bearings[j]) <= 225 && Math.abs(bearings[i] - bearings[j]) >= 135)
+                                ) {
+                                    bearings[i] += 9.2 * multiplier;
+                                    if (bearings[i] >= 360) bearings[i] -= 360;
+                                } else {
+                                    bearings[i] += 1;
+                                    if (bearings[i] >= 360) bearings[i] -= 360;
+                                }
+                            }
+                        } else if (p1.getLocation().distance(p2.getLocation()) <= 50) {
                             double newBearing = Player.calculateBearing(planes.get(i).getLocation(), planes.get(i).getDestination()) + 10;
                             double multiplier = planes.get(i).getLocation().distance(planes.get(i).getDestination());
                             multiplier /= 1 + planes.get(i).getLocation().distance(planes.get(i).getDestination());
 
-                            if (Math.abs(newBearing - bearings[i]) >= 10) { bearings[i] += 9.2 * multiplier; }
+                            if (Math.abs(newBearing - bearings[i]) >= 10) {
+                                Line2D line1 = new Line2D.Double(p1.getLocation().getX(), p1.getLocation().getY(), p1.getDestination().getX(), p1.getDestination().getY());
+                                Line2D line2 = new Line2D.Double(p2.getLocation().getX(), p2.getLocation().getY(), p2.getDestination().getX(), p2.getDestination().getY());
+
+                                if (line2.intersectsLine(line1) && Math.abs(bearings[i] - bearings[j]) <= 225 && Math.abs(bearings[i] - bearings[j]) >= 135) {
+                                    bearings[i] += 9.4 * multiplier;
+                                    if (bearings[i] >= 360) bearings[i] -= 360;
+                                } else {
+                                    bearings[i] += 1;
+                                    if (bearings[i] >= 360) bearings[i] -= 360;
+                                }
+                            } else {
+                                bearings[i] += 1;
+                                if (bearings[i] >= 360) bearings[i] -= 360;
+                            }
                         }
                     }
                 }
